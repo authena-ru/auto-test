85aa2ea00807bb9f4b99e101d3e4f301e31ce13a
djerys <kotukhov99@gmail.com>
2021-07-15 00:11:34 +0300

    Add basic initial server listener

--- cmd/autotest/main.go
+++ cmd/autotest/main.go
@@ -1,7 +1,29 @@
 package main
 
-import "log"
+import (
+	"log"
+	"net"
+	"os"
+
+	"google.golang.org/grpc"
+
+	"github.com/authena-ru/auto-test/internal/server"
+	"github.com/authena-ru/auto-test/internal/service"
+)
 
 func main() {
-	log.Print("auto-test")
+	lis, err := net.Listen("tcp", ":"+os.Getenv("PORT"))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", lis)
+	}
+
+	servReg := grpc.NewServer()
+	serv := server.New(service.CheckAttemptToPassTestingTask)
+	server.RegisterAutoTestServiceServer(servReg, serv)
+
+	log.Printf("server listening at %v", lis.Addr())
+
+	if err := servReg.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
